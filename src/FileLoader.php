0df624781c877cb233ac94daacb42827919d7d79
arisv <inquizgf@gmail.com>
2017-01-12 06:03:26 +0200

    Some actual code in here

---
+++ src/FileLoader.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Meow
+{
+    use \Silex\Application;
+    use \Symfony\Component\HttpFoundation\Request;
+    use \Symfony\Component\HttpFoundation\File;
+
+
+    class FileLoader
+    {
+        public function AddNewFile(Request $request, Application $app)
+        {
+            if($request->files->has('meowfile'))
+            {
+                $file = $request->files->get('meowfile');
+                if(!empty($file) && $file->isValid())
+                    return $file->getClientOriginalName();
+                else
+                    return "No file specified";
+            }
+            return "Wtf";
+        }
+    }
+}
--- web/app.php
+++ web/app.php
@@ -4,6 +4,17 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/../views'
+));
+$app['debug'] = true;
+
+
+$app->get('/', function() use ($app) {
+    return $app['twig']->render('homepage.twig');
+});
+
+$app->post('/getfile/','Meow\\FileLoader::AddNewFile');
 
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name);
